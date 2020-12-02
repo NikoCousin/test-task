8fd3398770afe480a4f3819b631bf9ebd64a853d
Niko <nikonikoyan21@gmail.com>
2020-12-03 01:29:37 +0400

    ..

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # test-task
+
 This is a test task
+
+Please type a comman in Your terminal:
+npm i;
+npm run start;
--- index.js
+++ index.js
@@ -10,6 +10,16 @@ sortable('.js-sortable-disabled', {
     el.click(function() {
         const table = $(`#table-${index + 1}`);
         const tr = $('#tempRow').clone();
+        let numb = $('.js-sortable-disabled').children().length;
+        // need to be fixed
+        tr.find('#javascript').attr('id',`javascript${numb + 10}`);
+        tr.find("[for='javascript']").attr('for',`javascript${numb + 10}`);
+        tr.find('#button-3').attr('id', `button-3${numb + 10}`);
+        tr.find('#toggle-3').attr('id', `toggle-3${numb + 10}`);
+        tr.find("[for='toggle-3']").attr('for', `toggle-3${numb + 10}`);
+        tr.find('#button-4').attr('id', `button-4${numb + 10}`);
+        tr.find('#toggle-4').attr('id', `toggle-4${numb + 10}`);
+        tr.find("[for='toggle-4']").attr('for', `toggle-4${numb + 10}`);
         appendRowToTable(table, tr);
     });
 })
